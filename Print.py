e40b1b73a94f82a32266a5f7d23ba2995396a5f2
Alex <amoderhack@students.mchenry.edu>
2025-02-28 08:32:28 -0600

    Added Python programs for conversion
    Print and hello world programs.

---
+++ Print.py
@@ -0,0 +1,9 @@
+# A knock knock joke program
+print("Knock, knock!")
+print("Who's there?")
+print("Omelette.")
+print("Omelette who?")
+print("Omelette myself in.")
+
+# My knock knock joke in a one line program
+print("Knock, knock.\nWho's there?\nOmelette.\nOmelette who?\nOmelette myself in.")
