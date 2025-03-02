f2d74263654e81a0e71813801b5e73c5bc961d76
Alex Moderhack <amoderhack@students.mchenry.edu>
2025-03-02 12:25:22 -0600

    updated mathematical program

--- Math.py
+++ Math.py
@@ -1,5 +1,10 @@
 #Program for calculating the area of a rectangle
+
+#length is length of rectangle
 length=10
+#width is width of rectangle
 width=6
+#area of rectangle is length multiplied by width
 area=length*width
+#print function prints the outcome of mathematical statement
 print("The area of a rectangle with a length of", length, "and a width of", width, "is", area)
