21bc70f78c5ffc21263dc1d42ffdff8c402fbe4d
Alex Moderhack <amoderhack@students.mchenry.edu>
2025-03-02 12:16:04 -0600

    data types commit

---
+++ Data_Types.py
@@ -0,0 +1,20 @@
+# This is a Python program to demonstrate different data types
+
+# Literal
+print("Literal:", 72)
+
+# Integer
+p = -72
+print("Integer:", p)
+
+# Float
+l = 44.4
+print("Float:", l)
+
+# String
+name = "Scottie"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
