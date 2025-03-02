007db7783865c1aa64b937aa9e8aeb1bba572628
Alex Moderhack <amoderhack@students.mchenry.edu>
2025-03-02 12:22:01 -0600

    Mathematical operations program

---
+++ Math.py
@@ -0,0 +1,5 @@
+#Program for calculating the area of a rectangle
+length=10
+width=6
+area=length*width
+print("The area of a rectangle with a length of", length, "and a width of", width, "is", area)
