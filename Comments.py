9ad658044a7bef5c30a3484e40fa781ccb9dd409
Alex Moderhack <amoderhack@students.mchenry.edu>
2025-03-02 12:59:12 -0600

    Comments added

---
+++ Comments.py
@@ -0,0 +1,15 @@
+# This is a program to explain the total mpg's of a vehicle
+
+# Total size of tank
+# tank_size is measured in gallons
+tank_size = 15
+
+# Define the total distance covered
+# distance is measured in miles
+distance = 600
+
+# Calculate miles per gallon
+mpg = distance / tank_size
+
+# Print the result
+print("Miles per gallon:", mpg)
