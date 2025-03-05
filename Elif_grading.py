163d44328e3523c34d055c09c29c2c4bc7502824
Alex Moderhack <amoderhack@students.mchenry.edu>
2025-03-05 09:15:53 -0600

    Updated grading program

--- Elif_grading.py
+++ Elif_grading.py
@@ -2,8 +2,16 @@
 user_grade=int(input("What is your numeric grade?: "))
 
 # Output for letter grade
+
+# Check if input is below zero
+if user_grade < 0:
+    print("Please input a valid grade.")
+# Check if input is above 100
+elif user_grade > 100:
+    print("Please input a valid grade.")
+
 #Check if grade is less than 60
-if user_grade < 60:
+elif user_grade < 60:
     print("Your letter grade is: F")
 #Check if grade is less than 70
 elif user_grade <= 69:
