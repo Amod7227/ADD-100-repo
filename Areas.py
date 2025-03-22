01de7723c32557a579477d4af2fae86af5a24ee5
Alex Moderhack <amoderhack@students.mchenry.edu>
2025-03-22 11:25:47 -0500

    reformatted circle and square program

--- Areas.py
+++ Areas.py
@@ -11,13 +11,18 @@ def circle(circle_area):
 
 # Input for length of side
 side = int((input("What is the length of your square's side?: ")))
+
 #input for Square units
 square_units = (input("What unit are you measuring your side in?: "))
+
 # Input for length of radius
 radius = (int(input("What is the radius of your circle?: ")))
+
 #input for radius units
 radius_units = (input("What unit are you measuring your radius in?: "))
+
 # Calls function and delivers output of area for square
 print(f"The area of your square is {square(side):.2f} square {square_units}.")
+
 # Calls function and delivers output for area of circle
 print(f"The area of your circle is {circle(radius):.2f} square {radius_units}.")
