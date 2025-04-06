f476300f3f92968163f280b7bff5465999778ad9
Alex Moderhack <amoderhack@students.mchenry.edu>
2025-04-06 09:42:56 -0500

    Tuple assignment

---
+++ tuples.py
@@ -0,0 +1,16 @@
+# Tuple for classes
+programming_classes_tuple = ("Intro to python", "Advanced Python", "Database essentials", "Web development basics", "Data structures in python", "Web design fundamentals",)
+
+# MAin function for program
+def Classes ():
+    programming_classes_tuple
+#Print function to describe the available classes
+    print ("The programming classes available are:")
+# For loop for each course in program
+    for Class in programming_classes_tuple:
+        print(Class)
+#print function to list the # of classes with 'len'
+    print("The program has", len(programming_classes_tuple), "available classes.")
+
+# Execution of main function
+Classes()
