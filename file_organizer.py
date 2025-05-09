a2945d41b0ede4517e1fa68d1824a5faeb6825f4
Alex Moderhack <amoderhack@students.mchenry.edu>
2025-05-09 08:18:29 -0500

    file_organizer

---
+++ file_organizer.py
@@ -0,0 +1,19 @@
+#import os
+import os
+
+#Main function to make directories
+def main():
+    #create MyFiles folder
+    os.mkdir("ADD-100-repo/MyFiles")
+    #create docs folder inside of MyFiles
+    os.mkdir("ADD-100-repo/MyFiles/Docs")
+    #create images folder inside of MyFiles
+    os.mkdir("ADD-100-repo/MyFiles/Images")
+    #create videos folder inside of MyFiles
+    os.mkdir("ADD-100-repo/MyFiles/Videos")
+
+
+
+
+#Call main function
+main()
